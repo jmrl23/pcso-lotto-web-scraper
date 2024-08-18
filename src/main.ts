ac2de3a48a2f995a149404d2ae099f1151abe46f
Jomariel Gaitera <gaiterajomariel@gmail.com>
2024-08-18 08:53:35 +0800

    chore: add information

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,3 @@
 import LottoScrapper from './LottoScrapper';
 
-async function main() {
-  const lottoScrapper = new LottoScrapper();
-  const results = await lottoScrapper.get({
-    from: 'August 1 2024',
-  });
-
-  console.log(results);
-}
-void main();
+export default LottoScrapper;
